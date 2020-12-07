f8e94d8d1efce82e9976b33a549f6c180bcfccd0
Nabil Pervez <perveznabil@gmail.com>
2020-12-06 18:03:06 -0600

    drawer

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,19 +1,172 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, ScrollView  } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
+import { createAppContainer } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 import Match from './MatchComponent';
 import TheLab from './TheLabComponent';
 import WarmUp from './WarmUpComponent';
 
+const MatchNavigator = createStackNavigator(
+    {
+        Match: { screen: Match }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='address-card'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
+const WarmUpNavigator = createStackNavigator(
+    {
+        WarmUp: { screen: WarmUp }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='address-card'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
+const TheLabNavigator = createStackNavigator(
+    {
+        TheLab: { screen: TheLab }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='address-card'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        WarmUp: {
+            screen: WarmUpNavigator,
+            navigationOptions: {
+                drawerLabel: 'Warm Up',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='home'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Match: {
+            screen: MatchNavigator,
+            navigationOptions: {
+                drawerLabel: 'Match',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='list'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        TheLab: {
+            screen: TheLabNavigator,
+            navigationOptions: {
+                drawerLabel: 'The Lab',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF',
+    }
+);
+
+const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
 
     render() {
         return (
-            <ScrollView>
-                <TheLab />
-            </ScrollView>
+            <View>
+                <AppNavigator/>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
+    },
+    stackIcon: {
+        marginLeft: 10,
+        color: '#fff',
+        fontSize: 24
+    }
+});
 
 export default Main;
